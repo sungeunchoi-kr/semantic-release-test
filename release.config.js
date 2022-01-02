99a30dab2b1c84a74e0b3c8d95893a24150af8e0
Sung E. Choi <sungeunchoi.kr@gmail.com>
2022-01-02 13:50:23 +0900

    Jobs (#2)
    * feat(jobs): Initial semantic-release setup
    
    * feat(jobs): resolve #1 - semantic-release setup
    - disable npm publish

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "branches": ["master"],
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [ "@semantic-release/npm",
+            {
+                "npmPublish": false,
+                "tarballDir": true
+            }
+        ],
+        "@semantic-release/github"
+    ]
+}
